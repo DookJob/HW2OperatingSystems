f8ea0d9031be21e9ac8552fb96ce1fc49e3cb099
Peter Green <57639888+DookJob@users.noreply.github.com>
2023-03-27 14:20:32 -0400

    smth
    smth

---
+++ MyScheduler.java
@@ -0,0 +1,17 @@
+/**
+ * Job Scheduler
+ * 
+ * Peter Green
+ * Patrick Marino
+ */
+
+import java.util.*;
+import java.util.concurrent.*; //for java.util.concurrent.BlockingQueue ??
+
+public class MyScheduler {
+    
+    //constructor
+    public MyScheduler(int numJobs, String property) {
+        //property can be "avg wait", "max wait", "combined"
+    }
+}
