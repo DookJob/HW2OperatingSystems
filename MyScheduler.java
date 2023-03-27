06d85ccc0cc51abbaeba9a26d7d4d1d838101673
Peter Green <57639888+DookJob@users.noreply.github.com>
2023-03-27 14:32:21 -0400

    finished outline

--- MyScheduler.java
+++ MyScheduler.java
@@ -14,4 +14,15 @@ public class MyScheduler {
     public MyScheduler(int numJobs, String property) {
         //property can be "avg wait", "max wait", "combined"
     }
+
+    //getter
+    public BlockingQueue<Job> getOutgoingQueue() {
+        //create outgoing queue in constructor?
+    }
+
+    //method
+    public getIncomingQueue() {
+        //returns BlockingQueue of Jobs
+        //call or create this in constructor?
+    }
 }
