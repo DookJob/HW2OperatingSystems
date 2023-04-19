7cdb34a3d9f8264c0e3db9350a789d915befb72d
PatMarino <57639980+Pm01967@users.noreply.github.com>
2023-04-19 16:27:50 -0400

    Minor changes

--- MyScheduler.java
+++ MyScheduler.java
@@ -10,29 +10,39 @@ import java.util.concurrent.*;
 
 public class MyScheduler {
 
-    private BlockingQueue outgoingQueue;
-    private BlockingQueue incomingQueue;
+    private BlockingQueue<Job> outgoingQueue;
+    private BlockingQueue<Job> incomingQueue;
+
+    private String timeType;
+    private int numJobs;
+    private int counter;
     
     
-    //constructor
+
     public MyScheduler(int numJobs, String property) {
-        //property can be "avg wait", "max wait", "combined"
+        this.timeType = property;
+        this.numJobs = numJobs;
+        this.outgoingQueue = new ArrayBlockingQueue<Job>(numJobs);
+        this.incomingQueue = new ArrayBlockingQueue<Job>(numJobs);
+        
     }
 
     //getter
     public BlockingQueue<Job> getOutgoingQueue() {
         //create outgoing queue in constructor?
+        return outgoingQueue;
     }
 
     //method
     public BlockingQueue<Job> getIncomingQueue() {
-        //returns BlockingQueue of Jobs
-        //call or create this in constructor?
+        return incomingQueue;
     }
 
     public void run() {
-        notifyAll();
+        
 
+
+        
 
     }
 }
