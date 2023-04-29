adb127eae55f5c8a6a97d579f16363327a36ab3a
Peter Green <petergreenboom8@gmail.com>
2023-04-28 22:36:37 -0400

    asd
    asd

--- MyScheduler.java
+++ MyScheduler.java
@@ -24,8 +24,16 @@ public class MyScheduler {
     public MyScheduler(int numJobs, String property) {
         this.timeType = property;
         this.numJobs = numJobs;
-        this.outgoingQueue = new ArrayBlockingQueue<Job>(numJobs);
         this.incomingQueue = new ArrayBlockingQueue<Job>(numJobs);
+
+
+        if(timeType == "max wait") {
+            this.outgoingQueue = new ArrayBlockingQueue<Job>(numJobs); // dont change, this seems to work most the time lol!
+        }else if(timeType == "avg wait") {
+            this.outgoingQueue = new PriorityBlockingQueue<Job>(numJobs, new jobComparator1());
+        } else { //combined
+            this.outgoingQueue = new ArrayBlockingQueue<Job>(numJobs);
+        }
     }
 
     //getter
@@ -41,46 +49,22 @@ public class MyScheduler {
 
 
     public void run() {
-
-        //first job goes straight into outgoing queue
-        try {
-            outgoingQueue.add(incomingQueue.take());
-        } catch(InterruptedException e) {
-            System.out.println(e);
-        }
-
-        // if(timeType == "max wait") {
-            PriorityBlockingQueue<Job> pbq1 = new PriorityBlockingQueue<Job>(numJobs, new jobComparator1());
-            for(int i = 1; i< numJobs; i++){
-                try {
-                    pbq1.add(incomingQueue.take());
-                }  catch(InterruptedException e) {
-                    System.out.println(e);
-                }
-                outgoingQueue.add(pbq1.poll());
+        for(int i = 0; i< numJobs; i++){
+            try {
+                outgoingQueue.add(incomingQueue.take());
+            } catch(InterruptedException e) {
+                System.out.println(e);
             }
-
-        // }else if(timeType == "avg wait") {
-        //     PriorityBlockingQueue<Job> pbq = new PriorityBlockingQueue<Job>();
-        //     for(int i = 1; i< numJobs; i++){
-                    
-        //     }
-
-        // }else { //combined
-        //     PriorityBlockingQueue<Job> pbq = new PriorityBlockingQueue<Job>();
-        //     for(int i = 1; i< numJobs; i++){
-                    
-        //     }
-                
-        // }
+            
+        }
     }
 }
 
 class jobComparator1 implements Comparator<Job>{
     public int compare(Job j1, Job j2) {
-        if(j1.getWaitTime() < j2.getWaitTime()){
+        if(j1.getLatency() > j2.getLatency()){
             return 1;
-        }else if(j1.getWaitTime() > j2.getWaitTime()){
+        }else if(j1.getLatency() < j2.getLatency()){
             return -1;
         }else{
             return 0;
